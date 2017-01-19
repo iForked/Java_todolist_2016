4d1704cafaa3e4c15040a855437a518af83d89bb
Theo Bertacchini <theo.bertacchini@epitech.eu>
2017-01-19 19:05:38 +0100

    Update

--- app/src/main/java/org/epitech/todolist/MainActivity.java
+++ app/src/main/java/org/epitech/todolist/MainActivity.java
@@ -1,38 +1,47 @@
 package org.epitech.todolist;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.StringDef;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.MultiTapKeyListener;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.appdatasearch.RegisterSectionInfo;
+import com.google.android.gms.tasks.Task;
+
+import org.epitech.todolist.db.TaskContract;
+import org.epitech.todolist.db.TaskDBHelper;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    FloatingActionButton fab;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
+    private FloatingActionButton fab;
+    private TaskDBHelper mHelper;
+    private ArrayAdapter<String> mAdapter;
+    private ListView mTaskListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Context c = this;
+        mHelper = new TaskDBHelper(c);
+        SQLiteDatabase db = mHelper.getReadableDatabase();
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -44,13 +53,22 @@ public class MainActivity extends AppCompatActivity {
                 final EditText userInput = (EditText) mView.findViewById(R.id.userInputDialog);
 
                 dialog.setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                Log.d("Input", "Task:" + userInput.getText().toString());
-                            }
-                        })
+                    public void onClick(DialogInterface dialog, int id) {
+                        Log.d("Input", "Task:" + userInput.getText().toString());
+                        String task = String.valueOf(userInput.getText().toString());
+                        SQLiteDatabase db = mHelper.getReadableDatabase();
+                        ContentValues values = new ContentValues();
+                        values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
+                        db.insertWithOnConflict(TaskContract.TaskEntry.Table,
+                                null,
+                                values,
+                                SQLiteDatabase.CONFLICT_REPLACE);
+                        db.close();
+                    }
+                })
                         .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
-                                Toast.makeText(c,"Add task canceled", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(c, "Add task canceled", Toast.LENGTH_SHORT).show();
                                 dialog.dismiss();
                             }
                         });
@@ -59,5 +77,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-}
 
+    private void updateUI() {
+        ArrayList<String> taskList = new ArrayList<>();
+        SQLiteDatabase db = mHelper.getReadableDatabase();
+        Cursor cursor = db.query(TaskContract.TaskEntry.Table,
+                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
+                null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            int id = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
+            taskList.add(cursor.getString(id));
+        }
+        if (mAdapter == null) {
+            mAdapter = new ArrayAdapter<>(this,
+                    R.layout.add_task,
+                    R.id.task,
+                    taskList);
+            mTaskListView.setAdapter(mAdapter);
+        } else {
+            mAdapter.clear();
+            mAdapter.addAll(taskList);
+            mAdapter.notifyDataSetChanged();
+        }
+        cursor.close();
+        db.close();
+    }
+}
